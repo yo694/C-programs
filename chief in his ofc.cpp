4adcc5f0cab8fdf8b1c9304a7cbee1c1ec08e2b6
Sai Yogitha Kilari <yogithakilari@gmail.com>
2024-07-05 22:13:36 +0530

    Add files via upload

---
+++ chief in his ofc.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    int t;
+    scanf("%d",&t);
+    while(t--){
+        int x,y;
+        scanf("%d%d",&x,&y);
+        printf("%d\n",(x*4)+y);
+    }
+	
+
+}
+
